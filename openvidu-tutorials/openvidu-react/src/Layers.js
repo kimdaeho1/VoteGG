7b212b9ee8d42de620dc9d704982dd40375f1338
Ubuntu <ubuntu@ip-172-31-7-177.ap-northeast-2.compute.internal>
2024-11-27 04:53:55 +0000

    EchoForce2

--- openvidu-tutorials/openvidu-react/src/Layers.js
+++ openvidu-tutorials/openvidu-react/src/Layers.js
@@ -8,6 +8,7 @@ import Signup from './components/Pages/Signup/Signup.jsx';
 import Header from './components/Shell/Header/Header.jsx';
 import LeftSidebar from './components/Shell/LeftSidebar/LeftSidebar.jsx';
 import './Layers.css';
+import DebateRoom from './components/Pages/DebateRoom/DebateRoom.jsx';
 
 const Layers = () => {
   return (
@@ -22,13 +23,21 @@ const Layers = () => {
               <Route path="/" element={<Home />} />
 
               {/* 테스트 */}
-            
+
 
               {/* 참여자와 관전자 라우트 */}
               <Route
                 path="/room/:roomNumber"
                 element={<Room role="participant" />}
               />
+
+
+              <Route
+                path="/DebateRoom/:roomNumber"
+                element={<DebateRoom role="participant" />}
+              />
+
+
               <Route
                 path="/observer/:roomNumber"
                 element={<Observer role="observer" />}
