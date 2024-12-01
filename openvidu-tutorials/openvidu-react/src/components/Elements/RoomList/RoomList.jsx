493f0826a03d7e8b5b0092f68ba4267b409c9c7c
Ubuntu <ubuntu@ip-172-31-4-137.ap-northeast-2.compute.internal>
2024-12-01 10:06:31 +0000

    feat:태그

--- openvidu-tutorials/openvidu-react/src/components/Elements/RoomList/RoomList.jsx
+++ openvidu-tutorials/openvidu-react/src/components/Elements/RoomList/RoomList.jsx
@@ -33,8 +33,8 @@ const RoomList = () => {
   // 검색어에 따라 필터링된 방 목록 생성
   const displayedRooms = searchQuery
     ? rooms.filter((room) =>
-      room.roomname.toLowerCase().includes(searchQuery.toLowerCase()) // 검색어와 방 이름 비교
-    )
+        room.roomname.toLowerCase().includes(searchQuery.toLowerCase()) // 검색어와 방 이름 비교
+      )
     : rooms; // 검색어가 없으면 전체 방 목록 표시
 
   // 총 페이지 수 계산
@@ -123,7 +123,6 @@ const RoomList = () => {
                   <div className="room-image">
                     <img
                       src={`${window.location.origin}${room.thumbnail}` || "./default-thumbnail.jpg"}
-                      // alt={`${room.roomname} 썸네일`}
                       onClick={() => navigate(`/observer/${room.roomNumber}`)}
                       className="entry-room"
                     />
@@ -150,8 +149,19 @@ const RoomList = () => {
                       토론하기
                     </button>
                   </div>
+                  {/* 태그 추가 */}
+                  <div className="room-tags">
+                    {room.tags && room.tags.length > 0 ? (
+                      room.tags.map((tag, idx) => (
+                        <span key={idx} className="tag-item">
+                          #{tag}
+                        </span>
+                      ))
+                    ) : (
+                      <span className="tag-placeholder">태그 없음</span>
+                    )}
+                  </div>
                 </div>
-
               )
             )}
           </div>
@@ -173,8 +183,9 @@ const RoomList = () => {
           ></span>
         ))}
       </div>
-    </div >
+    </div>
   );
 };
 
 export default RoomList;
+
